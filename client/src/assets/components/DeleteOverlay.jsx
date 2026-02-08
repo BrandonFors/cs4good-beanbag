f2d41a284759ccd35b2fce7fa0f921b6868d751b
BeegScoop <bfclashman15@gmail.com>
2026-02-08 11:41:02 -0500

    combined team/admin page

--- client/src/assets/components/DeleteOverlay.jsx
+++ client/src/assets/components/DeleteOverlay.jsx
@@ -6,31 +6,18 @@ function DeleteOverlay(props) {
   const [textContent, setTextContent] = useState("");
   const [buttonEnabled, setButtonEnabled] = useState(false);
 
-  useEffect(() => {
-    textContent == props.teamName
-      ? setButtonEnabled(true)
-      : setButtonEnabled(false);
-  }, [textContent]);
-
 
   return (
     <div className="delete-overlay">
       <div className="delete-container">
         <div className="delete-header">
-          <h1>Enter the Team's Name to Confirm Delete</h1>
+          <h1>Delete {props.teamName}?</h1>
           <a onClick={props.handlePopExit} className="close-button">
             <img src="/icons/close.svg" />
           </a>
         </div>
-        <h3>Case Sensitive</h3>
-        <input
-          type="text"
-          value={textContent}
-          onChange={(e) => setTextContent(e.target.value)}
-          placeholder="Enter team name"
-        />
-        <button onClick={()=>{props.handleConfirm(textContent)}} disabled={!buttonEnabled}>
-          Confirm
+        <button onClick={()=>{props.handleConfirm(textContent)}}>
+          Yes
         </button>
       </div>
     </div>
--- client/src/assets/components/TeamPage.jsx
+++ client/src/assets/components/TeamPage.jsx
@@ -2,11 +2,14 @@ import React, { useState, useEffect } from "react";
 import NavBar from "./NavBar";
 import "../styles/index.css";
 import axios from "axios";
+import DeleteOverlay from "./DeleteOverlay";
 
 function TeamPage() {
   const [teamName, setTeamName] = useState("");
+  const [teamNameDelete, setTeamNameDelete] = useState("");
   const [message, setMessage] = useState(""); // Feedback message
   const [teamList, setTeamList] = useState([]); // List of registered teams
+  const [isConfirming, setIsConfirming] = useState(false);
 
   // Fetch existing teams on load
   useEffect(() => {
@@ -16,6 +19,28 @@ function TeamPage() {
     };
     fetchTeamNames();
   }, []);
+
+  const handleDelete = (target) => {
+    setTeamNameDelete(target.name);
+    setIsConfirming(true);
+  };
+
+  const handlePopExit = () => {
+    setIsConfirming(false);
+  };
+
+  const handleDeleteConfirm = async (name) => {
+    let newTeamList = [];
+    const data = {
+      name: name,
+    };
+    const response = await axios.post(
+      "https://cs4good-beanbag-codt.onrender.com/delete_team",
+      data
+    );
+    setTeamList((prevValue) => prevValue.filter((team) => team !== name));
+    handlePopExit();
+  };
 
   const handleRegisterTeam = async () => {
     if (!teamName.trim()) {
@@ -54,18 +79,35 @@ function TeamPage() {
           <button onClick={handleRegisterTeam}>Register</button>
         </div>
 
-        <p className="message">{message}</p>
-
         <div className="teamList">
           <h2>Registered Teams</h2>
-          <ul>
-            {teamList.map((team, index) => (
-              <li key={index}>{team}</li>
-            ))}
-          </ul>
+
+          {teamList.map((team, index) => (
+            <div key={index} className="delete-element">
+              <button
+                onClick={(event) => {
+                  handleDelete(event.target);
+                }}
+                name={team}
+                value={team}
+              >
+                Delete
+              </button>
+              <h2>{team}</h2>
+            </div>
+          ))}
         </div>
       </div>
+      {isConfirming && (
+        <DeleteOverlay
+          teamName={teamNameDelete}
+          handlePopExit={handlePopExit}
+          handleConfirm={handleDeleteConfirm}
+        />
+      )}
+      <p className="message">{message}</p>
     </div>
+    
   );
 }
 
